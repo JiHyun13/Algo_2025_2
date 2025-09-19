1184a7ca8b9f223a04ead045bc8d60066bff5892
Jihyun13 <boksul1104@naver.com>
2025-09-19 23:30:44 +0900

    4주차까지

---
+++ Algo_2025_2/4-1 힙정렬 유일키.c
@@ -0,0 +1,88 @@
+//#pragma warning(disable:4996)
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int H[100];
+//int n = 0;
+//
+//void insertItem(int key);
+//void upHeap(int i);
+//void downHeap(int i);
+//void inPlaceHeapSort();
+//void rBuildHeap(int i);
+//void buildHeap();
+//void printArray();
+//
+//int main() {
+//
+//	int N, key;
+//	scanf("%d", &N);
+//
+//	for (int i = 1; i <= N; i++) {
+//		scanf("%d", &key);
+//		insertItem(key);
+//	}
+//
+//	inPlaceHeapSort();
+//	printArray();
+//}
+//
+//void insertItem(int key) {
+//	if (n > 100) return;
+//	H[++n] = key;
+//	upHeap(n);
+//}
+//
+//void upHeap(int i) {
+//	while (i > 1) {
+//		int p = i / 2;
+//		if (H[p] >= H[i]) break;
+//		int tmp = H[p]; H[p] = H[i]; H[i] = tmp;
+//		i = p;
+//	}
+//}
+//
+//void downHeap(int i) {
+//	while (1) {
+//		int left = i * 2;
+//		int right = i * 2 + 1;
+//		int t = i;
+//		//left든 right 든 n보다 작으면서(범위벗어나면x) 값 자체도 t 보다 클 때 
+//		if (left <= n && H[left] > H[t]) t = left;
+//		if (right <= n && H[right] > H[t]) t = right;
+//		if (t == i) break; //교환안되면 멈춤
+//
+//		int tmp = H[t]; H[t] = H[i]; H[i] = tmp;
+//		i = t;
+//	}
+//}
+//
+//void inPlaceHeapSort() {
+//	
+//	int N = n;
+//
+//	buildHeap();
+//
+//	for (int i = n; i >= 2; i--) {
+//		int tmp = H[1]; H[1] = H[i]; H[i] = tmp;
+//		n--;
+//		downHeap(1);
+//	}
+//
+//	n = N;
+//}
+//
+//void printArray() {
+//	for (int i = 1; i <= n; i++) printf(" %d", H[i]);
+//}
+//
+//void rBuildHeap(int i) {
+//	if (i < n) return;
+//	rBuildHeap(i * 2);
+//	rBuildHeap(i * 2 + 1);
+//	downHeap(i);
+//}
+//
+//void buildHeap() {
+//	for (int i = n / 2; i >= 1; i--) downHeap(i);
+//}
